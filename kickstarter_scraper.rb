d2688d0e86c5522486d9eaeb0f64aec87f5a93fc
Andrew Silva <andrewsilva9@gmail.com>
2019-05-30 00:43:55 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,15 @@
 # require libraries/modules here
+require 'nokogiri'
+
+require 'pry'
 
 def create_project_hash
+  html = File.read('fixtures/kickstarter.html')
+
+  kickstarter = Nokogiri::HTML(html)
   # write your code here
 end
+
+binding.pry
+
+create_project_hash
