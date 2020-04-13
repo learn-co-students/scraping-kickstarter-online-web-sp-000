5808af6f2116e39755f1fb435ca28766e2e06557
C. Leow <chienleow1@gmail.com>
2020-04-13 18:32:39 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,9 +1,21 @@
-require 'nokogiri'
-
-def create_project_hash
-  
-  html = File.read('fixtures/kickstarter.html')
+require "nokogiri"
+require "pry"
  
-  kickstarter = Nokogiri::HTML(html)
+def create_project_hash
+  projects = {}
+  
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
+  end
+  
+  projects
   
 end
+
+# projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
+# location: project.css("span.location-name").text
+
