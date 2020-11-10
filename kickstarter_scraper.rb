a5f2e32a1dd084704283a6b735d8870df40e9abf
Damian Malysza <damian.malysza@gmail.com>
2020-11-10 18:59:11 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,14 +2,11 @@ require 'nokogiri'
 require 'pry'
 
 def create_project_hash
-  # write your code here
   doc = Nokogiri::HTML(File.read("./fixtures/kickstarter.html"))
-  projects = doc.css(".project-card") #nokogiri array of projects (all details)
-  project_names = projects.css(".bbcard_name").css("a") #nokogiri array of project names
+  projects = doc.css(".project-card")
 
-  return_hash = {} #placeholder for return_hash - eventually
+  return_hash = {}
 
-  #iterates through the nokogiri array of project names and adds titles as keys set to value of empty array
   projects.each do |project|
     return_hash[project.css(".bbcard_name").css("a").text] = {
       description: project.css(".bbcard_blurb").text.strip,
@@ -21,13 +18,4 @@ def create_project_hash
 
   return_hash
 
-  # TO-DO --> identify css selectors for the various parameters needed, iterate through return hash and add those in as values
-  # hashes should look like this in the end:
-
-          ##title => {
-              # image_link => link
-              # desciption => "description"
-              # location => "location"
-              # percent_funded => integer
-            # }
 end
