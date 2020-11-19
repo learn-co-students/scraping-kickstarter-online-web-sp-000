263c4bd87150d210821d5b2fc2cf83f64b6cb8d2
fut0k0 <cccabell@icloud.com>
2020-11-19 15:51:35 -0800

    Passing lab

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,16 @@
-# require libraries/modules here
+require "nokogiri"
 
 def create_project_hash
-  # write your code here
+	doc = Nokogiri::HTML(File.read("./fixtures/kickstarter.html"))
+
+	Hash.new.tap do |hash|
+		doc.css("#projects_list .project.grid_4").each do |project|
+			hash[project.css(".bbcard_name strong a").text.strip.to_sym] = {
+				image_link: project.css(".project-thumbnail a img").attribute("src").value,
+				description: project.css(".bbcard_blurb").text.strip,
+				location: project.css(".project-meta li a .location-name").text.strip,
+				percent_funded: project.css(".project-stats .first.funded strong").text.strip[0...-1].to_i
+			}
+		end
+	end
 end
