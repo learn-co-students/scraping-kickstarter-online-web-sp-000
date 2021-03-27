fc7686f758d76b8fa7ed326b14918fb0defc849d
HELEN HARMAN <hlharman@bellsouth.net>
2021-03-27 23:35:37 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,28 +9,26 @@ require 'pry'
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
-  doc = Nokogiri::HTML(html) 
+  kickstarter = Nokogiri::HTML(html)
 end 
 
 
 def get_projects 
   projects = get_page.css(".li.project.grid_4")
 end 
+   
 
-
- def create_project_hash
-  projects = create_project_hash.css("li.project.grid_4").first 
- end 
+ 
  
  def make_projects 
-#   create_project_hash("a")
-# image_link = projectphoto - little
-# description 
-# location 
-# percent funded 
+   projects:  
+# image_link = img.projectphoto-little 
+# description = p.bbcard-blurb 
+# location = span.location-name 
+# percent funded = li.first.funded 
+#page = li.project.grid-4 
+# title = h2.bbcard_name strong a.text  
  end 
- 
-# projects: kickstarter.css("li.project.grid_4") 
- 
 
+create_project_hash 
  
