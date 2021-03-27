90bc50b1c49e9e728f8344b4d801309e80e1609d
HELEN HARMAN <hlharman@bellsouth.net>
2021-03-27 00:42:52 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,10 +1,27 @@
 require "nokogiri"
-require "pry"
- 
+require 'pry'
+
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
-  kickstarter = Nokogiri::HTML(html)
-  binding.pry
+  doc = Nokogiri::HTML(html) 
+  binding.pry 
 end
+
+
+
+
+ def create_project_hash
+ projects = create_project_hash.css("li.project.grid_4").first 
+ end 
  
-# projects: kickstarter.css("li.project.grid_4")
+ def make_projects 
+   create_project_hash("a")
+ image_link = projectphoto - little
+ description 
+ location 
+ percent funded
+ 
+# projects: kickstarter.css("li.project.grid_4") 
+ 
+
+ 
